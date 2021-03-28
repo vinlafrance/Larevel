37f4b6e30f8b241bb3edb299298d26c99e4ac5b1
Andrinirina Joël Velonjatovo <andrinirina-joel.velonjatovo.1@ulaval.ca>
2021-03-28 00:01:28 -0400

    script

---
+++ MySQL/script.py
@@ -0,0 +1,23 @@
+import random
+
+# ouvre aleatoire.txt en mode lecture
+# change le contenu pour que ca devient une liste
+with open('aleatoire.txt', 'r') as aleatoire:
+    listetitre = aleatoire.read().splitlines()
+
+with open('auteurs.txt', 'r') as auteurs:
+    listeauteur = auteurs.read().splitlines()
+
+genre = ['Roman', 'Polar', 'Theatre', 'Poesie', 'Science-fiction']
+annee = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
+
+# ouvre le fichier fichier.txt qui se trouve dans le meme dossier que ce .py,  ou le créer si ce fichier n'existe pas
+# en mode ecriture
+# OU f = open('fichier.txt', 'w')
+#    f.close()  a la fin
+with open('fichier.txt', 'w') as f:
+    for i in range(1, 101):
+        ligne = "(" + str(i) + ", " + '''"''' + random.choice(listetitre) + '''"''' + ", " + '''"''' + random.choice(
+            listeauteur) + '''"''' + ", " + '''"''' + random.choice(genre) + '''"''' + ", " + random.choice(
+            annee) + "),\n"
+        f.write(ligne)
