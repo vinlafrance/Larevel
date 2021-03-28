b46b8e1a745fcc9550e18ab714b59dad5c8353b5
Andrinirina Joël Velonjatovo <andrinirina-joel.velonjatovo.1@ulaval.ca>
2021-03-28 00:12:23 -0400

    script

--- MySQL/scriptCatalogue.py
+++ MySQL/scriptCatalogue.py
@@ -11,7 +11,7 @@ with open('auteurs.txt', 'r') as auteurs:
 genre = ['Roman', 'Polar', 'Theatre', 'Poesie', 'Science-fiction']
 annee = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
 
-# ouvre le fichier fichier.txt qui se trouve dans le meme dossier que ce .py,  ou le créer si ce fichier n'existe pas
+# ouvre fichier.txt qui se trouve dans le meme dossier que ce .py,  ou le créer si ce fichier n'existe pas
 # en mode ecriture
 # OU f = open('fichier.txt', 'w')
 #    f.close()  a la fin
